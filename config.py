02916c02e5ff69e76f2af64958eaabe0145b6f2b
Igor Varyvoda <cheguevaraua@gmail.com>
2018-03-16 13:06:56 +0100

    config change

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Hey {}, It great to connect
-			with you on twitter'''.format(name)
+	return '''Hey {}, thanks for the follow! While you're here, could you tell us one thing that'd ease your pain when handling images? We're hunting for new ways to improve Sirv in 2018.'''.format(name)
 
 scheduler_time = 15 #in minutes
 
-tw_username = "paraazz" #change this to yours
+tw_username = "wesirv" #change this to yours
